588ab6b817702574666d58cbbd86ce25fe3532b1
Antonin <antonin.garret@ens-rennes.fr>
2016-02-26 14:44:06 +0100

    added ia_average function (check_colors function apppending)

--- 7colors.c
+++ 7colors.c
@@ -10,58 +10,51 @@
 #define BOARD_SIZE 30
 #define NUMBER_OF_COLOR 7
 
-
-
-
 /* fonctions auxilliaires */
 void assert(int check, int line){
     if (!check){
         printf("error at line %i\n", line);
         exit(1);
     }
-} 
+}
 
-/** Represent the actual current board game 
- * 
+/** Represent the actual current board game
+ *
  * NOTE: global variables are usually discouraged (plus encapsulation in
- *  an appropriate data structure would also be preferred), but don't worry. 
- *  For this first assignment, no dinosaure will get you if you do that. 
+ *  an appropriate data structure would also be preferred), but don't worry.
+ *  For this first assignment, no dinosaure will get you if you do that.
  */
 char board[BOARD_SIZE * BOARD_SIZE] = { 0 }; // Filled with zeros
 
 /** Retrieves the color of a given board cell */
-char get_cell(int x, int y) 
+char get_cell(int x, int y) // O(1)
 {
    return board[y*BOARD_SIZE + x];
 }
 
 /** Changes the color of a given board cell */
-void set_cell(int x, int y, char color) 
+void set_cell(int x, int y, char color) // O(1)
 {
    board[y*BOARD_SIZE + x] = color;
 }
-   
-/** Prints the current state of the board on screen
- * 
- * Implementation note: It would be nicer to do this with ncurse or even 
- * SDL/allegro, but this is not really the purpose of this assignment.
- */
-void print_board() 
+
+void print_board() // O(BOARD_SIZE²)
 {
    int i, j;
+   system("clear");
    for (i=0; i<BOARD_SIZE; i++) {
-      for (j=0; j<BOARD_SIZE; j++) 
+      for (j=0; j<BOARD_SIZE; j++)
 	    printf("%i ", get_cell(i, j)); // à modifier pour rendre compatible avec des chars
           printf("\n");
    }
 }
 
-int random_color(){
+int random_color(){ // Choose a random color != than 0 or 1
   return ((rand()%NUMBER_OF_COLOR)+2);
 }
 
-void initgame(){
-  int buff; // On crée un buffer pour l'aléatoire
+void initgame(){// Create board, O(BOARD_SIZE²)
+  int buff; // Random buffer
   int i,j;
   for (i=0;i<BOARD_SIZE;i++){
     for (j=0;j<BOARD_SIZE;j++){
@@ -70,17 +63,17 @@ void initgame(){
     }
   }
   set_cell(0,BOARD_SIZE-1,0);
-  set_cell(BOARD_SIZE-1,0,1);  
+  set_cell(BOARD_SIZE-1,0,1);
 }
 
-void coup(int joueur, int color){
-    assert(joueur <= 1, __LINE__);
-    assert(color >= 2 && (color <= NUMBER_OF_COLOR + 2), __LINE__);
-    char board_temp[BOARD_SIZE * BOARD_SIZE] = { 0 };
+void coup(int joueur, int color){// Make a shot, good complexity
+    assert(joueur <= 1, __LINE__); // Never suppose to have this error
+    assert(color >= 2 && (color <= NUMBER_OF_COLOR + 2), __LINE__); // this one too
+    char board_temp[BOARD_SIZE * BOARD_SIZE] = { 0 }; // this made a o(BOARD_SIZE²) spacial complexity
     void step_coup(int x, int y){
         if (x >= 0 && x <= BOARD_SIZE-1 && y>=0 && y <= BOARD_SIZE-1 && (get_cell(x,y) == joueur || get_cell(x,y) == color) && board_temp[y*BOARD_SIZE + x] == 0){
             set_cell(x,y,joueur);
-            board_temp[y*BOARD_SIZE + x] = 1; // on indique qu'on a deja visité la case
+            board_temp[y*BOARD_SIZE + x] = 1; // Marker to avoid infinite loop
             step_coup(x-1,y);
             step_coup(x,y-1);
             step_coup(x+1,y);
@@ -93,20 +86,56 @@ void coup(int joueur, int color){
         step_coup(BOARD_SIZE-1,0);
 }
 
-char player(){ // doit verifier la couleur
+char player(){ // Have to check that return is correct
     int color;
-    printf("Choisi ta couleur entre 2 et 9\n");
-    scanf("%d",&color);
-    assert(color >=2 && color <= 9,__LINE__); // assert temporaire, à modifier avec une boucle qui attend une vrai couleur
+    do { // loop to get the color
+    	printf("Choisi ta couleur entre 2 et 9\n"); // Hard Coded
+    	scanf("%d",&color);
+    } while (color < 2 || color > NUMBER_OF_COLOR +2);
     return color;
 }
 
-char ia_aleatoire(){
+char ia_verybad(){ // Simple random choice (very stupid)
     return random_color();
 }
-   
 
-char gamenotend(){
+char ia_average(int player){
+	int colors_best[NUMBER_OF_COLOR +2] = { 0 };
+	char board_temp[BOARD_SIZE * BOARD_SIZE] = { 0 };
+    int i;
+	void step_coup(int x, int y){
+		if ( x >= 0 && x <= BOARD_SIZE -1 && y >= 0 && y <= BOARD_SIZE-1 && board_temp[y*BOARD_SIZE + x] == 0){
+			board_temp[y*BOARD_SIZE +x] = 1;
+			if (get_cell(x,y) == player){
+				step_coup(x-1,y);
+				step_coup(x,y-1);
+				step_coup(x+1,y);
+				step_coup(x,y+1);
+			}
+			else {
+				colors_best[(int)(get_cell(x,y))] += 1; // change to += 1 to have a smart one, == 1 to have a bad one
+				// check_color(get_cell(i,j),x,y) // Uncomment to have a better one
+			}
+		}
+	}
+	if (player == 0){
+		step_coup(0,BOARD_SIZE -1);
+	}
+	else {
+		step_coup(0,BOARD_SIZE -1);
+	}
+	int color;
+    int m = 0;
+    for (i=2;i<NUMBER_OF_COLOR +2;i++){
+        if (colors_best[i] > m){
+                color = i;
+                m = [i];
+        }
+    }
+    return color;
+}
+
+char gamenotend(){ // check if the game is over, O(BOARD_SIZE²)
     int nombre_de_case = BOARD_SIZE * BOARD_SIZE;
     int case_joueur0 = 0;
     int case_joueur1 = 0;
@@ -128,29 +157,27 @@ char gamenotend(){
         return 0;
     }
     return 1;
-    
+
 }
 
 /** Program entry point */
-int main() 
+int main()
 {
-    srand(time(NULL)); // initialise le seed
+    srand(time(NULL)); // seed initialisation
     char col = 0;
     char turn_player;
     initgame();
     printf("\n\n  Welcome to the 7 wonders of the world of the 7 colors\n"
 	      "  *****************************************************\n\n"
 	"Current board state:\n");
-   
     print_board();
-    printf("Joueur 0 commence\n");
-    for(turn_player = 0;gamenotend(); turn_player = 1-turn_player){
+    printf("Joueur 0 commence\n"); // hard coded player 0 start (don't blame me please)
+    for(turn_player = 0;gamenotend(); turn_player = 1-turn_player){ // While game is not over
         if (turn_player == 0)
-            col = ia_aleatoire(); // on sait que coup est une couleur valide
+            col = ia_average(0); // first player
         else
-            col = ia_aleatoire();
-        coup(turn_player,col);
-        system("clear");
+            col = ia_verybad(); // other player
+        coup(turn_player,col); // make the play
         print_board();
     }
     return 0; // Everything went well
